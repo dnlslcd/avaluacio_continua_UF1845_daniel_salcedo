dd16cc53ef5a7f63c2e635344d2bea7dba5b0ccc
dnlslcd <danielsalcedoq@gmail.com>
2024-09-30 17:59:52 +0200

    Detalle de un apartamento - empezado

--- models/apartment.model.js
+++ models/apartment.model.js
@@ -25,17 +25,17 @@ const apartmentSchema = new Schema({
         type: String,
         required: true,
     },
-    services: {
-        // array de strings 
-        // ["wifi", "air aconditionar"]
-        // objeto con los servicios { wifi: true, airConditioner: false}
-        wifi: Boolean,
-        airConditioner: Boolean,
-        kitchen: Boolean,
-        disability: Boolean,
-        heater: Boolean,
-        tv: Boolean
-    },
+    // services: {
+    //     // array de strings 
+    //     // ["wifi", "air aconditionar"]
+    //     // objeto con los servicios { wifi: true, airConditioner: false}
+    //     wifi: Boolean,
+    //     airConditioner: Boolean,
+    //     kitchen: Boolean,
+    //     disability: Boolean,
+    //     heater: Boolean,
+    //     tv: Boolean
+    // },
     // services: {
     //     type: [String], // enum
     //     validate: {
