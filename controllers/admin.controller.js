76e5efdd14882a8c99cca43473db891428a2026e
dnlslcd <danielsalcedoq@gmail.com>
2024-09-30 19:49:29 +0200

    obtener demás campos del req.body

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -10,7 +10,12 @@ const postNewApartment = async (req, res) => {
         description: req.body.description,
         price: req.body.price,
         size: req.body.size,
-        mainPhoto: req.body.mainPhoto
+        mainPhoto: req.body.mainPhoto,
+        numberRooms: req.body.numberRooms,
+        numberTotalBeds: req.body.numberTotalBeds,
+        numberBathrooms: req.body.numberBathrooms,
+        maxCapacity: req.body.maxCapacity,
+        houseRules: req.body.houseRules
     });
 
     res.send('Apartment created')
