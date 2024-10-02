5cc2373dc656ae37872d7f730011943b2331d868
dnlslcd <danielsalcedoq@gmail.com>
2024-10-02 18:17:52 +0200

    nuevos campos añadidos en modelo y formulario

--- models/apartment.model.js
+++ models/apartment.model.js
@@ -25,17 +25,14 @@ const apartmentSchema = new Schema({
         type: String,
         required: true,
     },
-    // services: {
-    //     // array de strings 
-    //     // ["wifi", "air aconditionar"]
-    //     // objeto con los servicios { wifi: true, airConditioner: false}
-    //     wifi: Boolean,
-    //     airConditioner: Boolean,
-    //     kitchen: Boolean,
-    //     disability: Boolean,
-    //     heater: Boolean,
-    //     tv: Boolean
-    // },
+    services: {
+        WiFi: Boolean,
+        airConditioning: Boolean,
+        kitchen: Boolean,
+        disabilityInclusive: Boolean,
+        heating: Boolean,
+        tv: Boolean
+    }
     // services: {
     //     type: [String], // enum
     //     validate: {
