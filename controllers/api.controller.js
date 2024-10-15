64a2f0f0904c9132d5cb2695798099fe22fd402e
dnlslcd <danielsalcedoq@gmail.com>
2024-10-15 20:41:44 +0200

    limitar nº apts en la query - hecho

--- controllers/api.controller.js
+++ controllers/api.controller.js
@@ -1,9 +1,20 @@
 const Apartment = require("../models/apartment.model");
 
 const getApartments = async (req, res) => {
-    // 1. ir al modelo y obtener todos los apartamentos (hasta 100.000)
-    const apartments = await Apartment.find().limit(100000);
-    // 2. devolver una respuesta json
+    // 2. Limitar apts
+    // 2.1. obtener de la query string el valor del parámetro limit
+    const limit = req.query.limit || 100000;
+
+    // 2.2. validaciones
+    if (limit < 1 || limit > 100000) {
+        return res.status(400).json({
+            message: "Limit parameter must be between 1 and 100000"
+        })
+    }
+
+    // 1.1. ir al modelo y obtener todos los apartamentos (hasta 100.000)
+    const apartments = await Apartment.find().limit(limit);
+    // 1.2. devolver una respuesta json
     return res.status(200).json({
         message: "Query excecuted succesfully",
         results: apartments
--- routes/api.route.js
+++ routes/api.route.js
@@ -3,6 +3,7 @@ const apiControllers = require('../controllers/api.controller')
 const express = require('express');
 const router = express.Router();
 
+// 1. Obtener todos los apartamentos
 router.get('/apartments', apiControllers.getApartments);
 
 module.exports = router;
