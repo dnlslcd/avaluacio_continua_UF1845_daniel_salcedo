beb62b9ca37de38cfc6deb2d973308436ce3fe3d
dnlslcd <danielsalcedoq@gmail.com>
2024-10-16 20:44:08 +0200

    express-validator

--- controllers/api.controller.js
+++ controllers/api.controller.js
@@ -1,4 +1,5 @@
 const Apartment = require("../models/apartment.model");
+const { validationResult } = require('express-validator');
 
 const getApartments = async (req, res) => {
     // 2. Limitar apts
@@ -6,12 +7,22 @@ const getApartments = async (req, res) => {
     const limit = req.query.limit || 100000;
 
     // 2.2. validaciones
-    if (limit < 1 || limit > 100000) {
-        return res.status(400).json({
-            message: "Limit parameter must be between 1 and 100000"
-        })
-    }
 
+    // if (limit < 1 || limit > 100000) {
+    //     return res.status(400).json({
+    //         message: "Limit parameter must be between 1 and 100000"
+    //     })
+    // }
+    
+    const result = validationResult(req);
+
+    // si validationResult devuelve algun valor (si no está vacío), es que ha habido algún parámetro que no ha pasado la validación
+    if (!result.isEmpty()){
+        return res.status(400).json({
+            message: result.array()[0].msg
+        });
+    };
+    
     // 1.1. ir al modelo y obtener todos los apartamentos (hasta 100.000)
     const apartments = await Apartment.find().limit(limit);
     // 1.2. devolver una respuesta json
--- routes/api.route.js
+++ routes/api.route.js
@@ -2,10 +2,18 @@ const apiControllers = require('../controllers/api.controller')
 
 const express = require('express');
 const router = express.Router();
+const { query } = require('express-validator');
 
 // 1. Obtener todos los apartamentos
-router.get('/apartments', apiControllers.getApartments);
+router.get('/apartments', query("limit")
+.optional()
+.isInt({ 
+    min: 1, 
+    max: 100000
+})
+.withMessage('"limit" parameter must be a number between 1 and 100.000'), 
+apiControllers.getApartments);
 
-// 3. Buscar apts por nombre/descripción
+// 3. Buscar apts por nombre/descripción   
 
 module.exports = router;
